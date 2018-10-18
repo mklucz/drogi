9f29fca5c75e867af51817b81d10247e56643bb2
Maciej Kluczyński <maciej.lukasz.kluczynski@gmail.com>
2018-10-18 23:15:14 +0200

    factor checking for cached extractout from __init__

--- drogi/waymap.py
+++ drogi/waymap.py
@@ -12,11 +12,10 @@ class WayMap:
     """Contains GIS-type data describing physical layout of ways in a particular
     area."""
 
-    def __init__(self, area, filename=None):
+    def __init__(self, area):
         """
         Args:
             area(str): geographical area to be fetched and turned into a map
-            filename(str): filename to save the fetched extract as.
         """
         self.area = area
         self.bounds_to_fetch = BOUNDS_DICT[area]
@@ -24,34 +23,48 @@ class WayMap:
         self.minlon = self.bounds_to_fetch[1]
         self.maxlat = self.bounds_to_fetch[2]
         self.maxlon = self.bounds_to_fetch[3]
-        self.filename_to_use = filename
+        self.filename_to_use = None
         self.oldfile_name = None
-        for existing_file in os.listdir(os.getcwd()):
+        self.extracts_path = os.getcwd() + '/extracts/'
+        self.check_for_cached_extract()
+
+        if self.filename_to_use is None:
+            if self.oldfile_name:
+                os.remove(self.extracts_path + self.oldfile_name)
+            curr_time = str(datetime.datetime.utcnow()).replace(" ", "_")
+            self.filename_to_use = self.area + curr_time + ".osm"
+
+            api = overpass.API(timeout=600)
+            map_query = overpass.MapQuery(self.minlat, self.minlon,
+                                          self.maxlat, self.maxlon)
+            response = api.get(map_query, responseformat="xml")
+            with open(self.extracts_path + self.filename_to_use, "w") as f:
+                f.write(response)
+
+        self.handler = OSMHandler(self.extracts_path + self.filename_to_use)
+        self.handler.apply_file(self.extracts_path + self.filename_to_use,
+                                locations=True)
+
+        self.way_list = self.handler.way_list
+        self.bounds = (self.minlat, self.minlon, self.maxlat, self.maxlon)
+        self.graph = Graph(WayGraph(self.way_list))
+
+    def fetch_extract(self):
+        pass
+
+    def check_for_cached_extract(self):
+
+        for existing_file in os.listdir(self.extracts_path):
             if existing_file.startswith(self.area) and existing_file.endswith(".osm"):
                 cached_extract_mtime = \
                     datetime.datetime.utcfromtimestamp(
-                        os.path.getmtime(existing_file))
+                        os.path.getmtime(self.extracts_path + existing_file))
                 if (datetime.datetime.utcnow() - cached_extract_mtime <
                         datetime.timedelta(days=7)):
                     self.filename_to_use = existing_file
                 else:
                     self.oldfile_name = existing_file
-        if self.filename_to_use is None:
-            # if self.oldfile_name:
-            #     os.remove(self.oldfile_name)
-            curr_time = str(datetime.datetime.utcnow()).replace(" ", "_")
-            self.filename_to_use = self.area + curr_time + ".osm"
-            api = overpass.API(timeout=600)
-            map_query = overpass.MapQuery(self.minlat, self.minlon,
-                                          self.maxlat, self.maxlon)
-            response = api.get(map_query, responseformat="xml")
-            with open(self.filename_to_use, "w") as f:
-                f.write(response)
-        self.handler = OSMHandler(self.filename_to_use)
-        self.handler.apply_file(self.filename_to_use, locations=True)
-        self.way_list = self.handler.way_list
-        self.bounds = (self.minlat, self.minlon, self.maxlat, self.maxlon)
-        self.graph = Graph(WayGraph(self.way_list))
+        
 
     def render_on_canvas(self, canvas, **kwargs):
         """
